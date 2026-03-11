e44400c2cab1a186aeaef7661bc162359c4cd748
nicolasviste <nicolasviste@gmail.com>
2026-03-10 23:50:47 -0700

    Add hero header bar

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,3 +1,15 @@
 export function HeroSection() {
-    return <section className="hero-section" />;
+    return (
+        <>
+            <section className="hero-section">
+                <nav className="navbar sticky">
+                    <section className="sticky-left">
+                        <div className="placeholder-image" />
+                        <span className="text">Lorem Ipsum</span>
+                    </section>
+                    <span className="text">→</span>
+                </nav>
+            </section>
+        </>
+    );
 }
